6222b95a46f9248b89a39c3c9939826028c691c8
Abdellah LHASSANI <abdellah@Firsts-iMac.local>
2018-03-01 10:46:51 +0000

    added network service class

---
+++ MovieBrowser/NetworkService.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkService.swift
+//  MovieBrowser
+//
+//  Created by Abdellah LHASSANI on 3/1/18.
+//  Copyright © 2018 Abdellah LHASSANI. All rights reserved.
+//
+
+import UIKit
+
+class NetworkService: NSObject {
+    
+    static func getAllMovies(apiUrl:String,completion : @escaping ([Movie]?) -> Void)
+    {
+        
+
+        
+    }
+
+}
