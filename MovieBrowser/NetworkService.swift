26a02873ad87a20f30ffa17c45308ccf1229c1ef
Abdellah LHASSANI <abdellah@Firsts-iMac.local>
2018-03-01 11:25:06 +0000

    Added getAllMovies function to NetworkService

--- MovieBrowser/NetworkService.swift
+++ MovieBrowser/NetworkService.swift
@@ -13,7 +13,61 @@ class NetworkService: NSObject {
     static func getAllMovies(apiUrl:String,completion : @escaping ([Movie]?) -> Void)
     {
         
+        var movieList:[Movie]?
+        
+        guard let url = URL(string: apiUrl) else {
+             print("Error: cannot create URL")
+            completion(nil)
+            return
+        }
+        
+        let urlRequest = URLRequest(url: url)
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: urlRequest) { (data, response, error) in
+            
+            guard error == nil else {
+                completion(nil)
+                return
+            }
+            
+            guard let responseData = data else
+            {
+                completion(nil)
+                return
+            }
+            
+            do{
+                
+                if let dataJson = try JSONSerialization.jsonObject(with: responseData, options: []) as? [String:Any]
+                {
+                    guard let results = dataJson["results"] as? [String] else {
+                        
+                        completion(nil)
+                        return
+                    }
+                    
+                    for i in 0..<results.count {
+                        
+                        if let  movie = Movie(json: (results[i] as? [String:Any])!) {
+                            print(movie)
+                              movieList?.append(movie)
+                        }
 
+                        
+                    }
+                    
+                }
+                
+            }catch
+            {
+                return
+            }
+            
+        }
+        
+        task.resume()
         
     }
 
