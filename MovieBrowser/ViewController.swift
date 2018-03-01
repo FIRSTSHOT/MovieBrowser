ec003e2f66ada743780b51e8cfe518fd0dddad1e
Abdellah LHASSANI <abdellah@Firsts-iMac.local>
2018-03-01 15:21:44 +0000

    Added reload data inside the main thread

--- MovieBrowser/ViewController.swift
+++ MovieBrowser/ViewController.swift
@@ -8,23 +8,32 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     var movies : [Movie]?
+    var selectedMovie: Movie?
+    
     @IBOutlet weak var tableView: UITableView!
     
     var api = "https://api.themoviedb.org/3/discover/movie?api_key=98ddab1a678013f4f420946ce3d8b605&language=en-US&sort_by=popularity.desc&page=1"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         tableView.delegate = self
         tableView.dataSource = self
+        
         getMovies { (data) in
             
             self.movies = data
             
-            self.tableView.reloadData()
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            
         }
+        
 
         
     }
@@ -42,6 +51,12 @@ class ViewController: UIViewController {
         }
     
         
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? MovieDetailViewController {
+            destination.selectedMovie = selectedMovie
+        }
     }
 
 
@@ -73,6 +88,16 @@ extension ViewController : UITableViewDelegate,UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if self.movies == nil {
+            return
+        }
+        selectedMovie = movies?[indexPath.row]
+        performSegue(withIdentifier: "showMovieDetailSegue", sender: self)
+        
     }
     
 }
