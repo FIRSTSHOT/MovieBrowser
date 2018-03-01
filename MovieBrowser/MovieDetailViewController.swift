ea230215792a79dcc4e9859d2434f1df553103c8
Abdellah LHASSANI <abdellah@Firsts-iMac.local>
2018-03-01 16:18:09 +0000

    Added a section for Favorite Movies and MovieDetailViewController delegate

--- MovieBrowser/MovieDetailViewController.swift
+++ MovieBrowser/MovieDetailViewController.swift
@@ -8,27 +8,59 @@
 
 import UIKit
 
-
+protocol MovieDetailViewControllerDelegate {
+    func selectedFavoriteMovie(favorite : Movie)
+}
 
 class MovieDetailViewController: UIViewController {
     
 
+    @IBOutlet weak var addToFavoritesButton: UIButton!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var overviewTextView: UITextView!
+    var isFavorite:Bool?
     
     var selectedMovie:Movie?
-    
+    var delegate : MovieDetailViewControllerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
         
         if(selectedMovie != nil)
         {
             titleLabel.text = selectedMovie?.title
             overviewTextView.text = selectedMovie?.overview
+            
+            if(isFavorite != nil)
+            {
+                addToFavoritesButton.setTitle("Movie Added", for: .normal)
+                addToFavoritesButton.isEnabled = false
+                
+            }
         }
+        
+        
     }
 
-
+    @IBAction func AddToFavorites(_ sender: Any) {
+        
+        if let movie = selectedMovie {
+         
+            delegate?.selectedFavoriteMovie(favorite: movie)
+            
+            addToFavoritesButton.setTitle("Movie Added", for: .normal) 
+            addToFavoritesButton.isEnabled = false
+            
+            
+        }
+        
+        
+        
+        
+    }
+    
 }
 
